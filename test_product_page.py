395e8f1d86af40300d5f6c06517a72e6953dec43
Voviizzz <ivovochka48@gmail.com>
2024-10-07 22:25:38 +0300

    Запускается тест с параметром, один из тестов с параметром 7 - ожидаемо упадет. Пометил его как x-fail.

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,11 @@
+import pytest
+
 from StepicSeleniumPython.pages.product_page import ProductPage
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('NumberPromo', ['0','1','2','3','4','5','6',pytest.param('7', marks=pytest.mark.xfail),'8','9'])
+def test_guest_can_go_to_login_page(browser, NumberPromo):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{NumberPromo}"
     page = ProductPage(browser, link)
     page.open()
     # page.should_be_product_url()
