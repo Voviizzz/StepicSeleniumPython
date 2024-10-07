fb9be89e0e1e2b4fd1adbdfbe05e0c23f1abb683
Voviizzz <ivovochka48@gmail.com>
2024-10-07 23:25:10 +0300

    Изменил локатор

--- pages/locators.py
+++ pages/locators.py
@@ -20,6 +20,5 @@ class LoginPageLocators():
 #Локаторы для продукта
 class ProductPageLocators():
     BUTTON_ADD_TO_CART = (By.CLASS_NAME, "btn-add-to-basket")
-    SUCSESS_ADD_TO_CART_TEXT = ' был добавлен в вашу корзину'
-
-    SUCSESS_ALLERT_ADD_TO_CART_TEXT = (By.XPATH, "//*[@class='alertinner ' and text()[contains(.,' был добавлен в вашу корзину')]]")
+    # SUCSESS_ADD_TO_CART_TEXT = ' был добавлен в вашу корзину'
+    SUCSESS_ALLERT_ADD_TO_CART = (By.XPATH, "//*[@class='alertinner ' and text()[contains(.,' был добавлен в вашу корзину')]]")
