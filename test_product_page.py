bebfa2625328a15556241ff5b78ae85854553cc0
Voviizzz <ivovochka48@gmail.com>
2024-10-07 23:25:49 +0300

    данные тесты проверяют отображаются ли элементы на странице

--- test_product_page.py
+++ test_product_page.py
@@ -3,11 +3,37 @@ import pytest
 from StepicSeleniumPython.pages.product_page import ProductPage
 
 
-@pytest.mark.parametrize('NumberPromo', ['0','1','2','3','4','5','6',pytest.param('7', marks=pytest.mark.xfail),'8','9'])
-def test_guest_can_go_to_login_page(browser, NumberPromo):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{NumberPromo}"
+# @pytest.mark.parametrize('NumberPromo', ['0','1','2','3','4','5','6',pytest.param('7', marks=pytest.mark.xfail),'8','9'])
+# def test_guest_can_go_to_login_page(browser, NumberPromo):
+#     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{NumberPromo}"
+#     page = ProductPage(browser, link)
+#     page.open()
+#     # page.is_disappeared()
+#     # page.should_be_product_url()
+#     page.add_to_cart()
+    # page.solve_quiz_and_get_code()
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket (browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     page = ProductPage(browser, link)
     page.open()
-    # page.should_be_product_url()
     page.add_to_cart()
-    # page.solve_quiz_and_get_code()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message (browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+def test_message_disappeared_after_adding_product_to_basket (browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_cart()
+    page.should_not_be_is_disappeared()
+
+
+
+
+
