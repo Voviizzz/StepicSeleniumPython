3d5b5e47014695f1474b6df2f8dc28fa98007824
Voviizzz <ivovochka48@gmail.com>
2024-10-08 23:37:16 +0300

    Добавил класс для проверки перехода на страницу авторизации

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,10 @@
+import pytest
+
 from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 from .pages.basket_page import BasketPage
 from selenium.webdriver.common.by import By
-# link = "https://selenium1py.pythonanywhere.com/ru/accounts/login/"
+link = "https://selenium1py.pythonanywhere.com/ru/accounts/login/"
 
 
 # Это не по pageObjectModel - необходимо вынести переходы, логику в отдельные функции
@@ -17,15 +19,18 @@ from selenium.webdriver.common.by import By
 #     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
 #     login_link.click()
 
-# def test_guest_can_go_to_login_page(browser):
-#     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-#     page.open()                      # открываем страницу
-#     page.go_to_login_page()
-#
-# def test_guest_should_see_login_link(browser):
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.should_be_login_link()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self,browser):
+        page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()                      # открываем страницу
+        page.go_to_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
 # def test_guest_can_go_to_login_page(browser):
 #     link = "http://selenium1py.pythonanywhere.com"
 #     page = MainPage(browser, link)
